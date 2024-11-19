5e338fbf13063bd513f732b47c53e216de963205
ishanrana363 <ishanrana363@gmail.com>
2024-11-19 08:31:02 +0600

    banner section responsive design completed

--- src/pages/home-page/Banner.jsx
+++ src/pages/home-page/Banner.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Banner = () => {
     return (
         <div>
-            <div className="bg-[url('https://res.cloudinary.com/dj2edy2rg/image/upload/v1731947888/Rectangle_2_pnbols.png')] bg-cover bg-center h-screen ">
-                <div className='flex justify-center items-center h-screen ' >
-                    <div className='w-1/3' >
+            <div className="bg-[url('https://res.cloudinary.com/dj2edy2rg/image/upload/v1731947888/Rectangle_2_pnbols.png')] bg-cover bg-center h-[50vh] md:h-[75vh] lg:h-screen">
+                <div className="flex justify-center items-center h-[50vh] md:h-[75vh] lg:h-screen">
+                    {/* Empty Space (for layout balance) */}
+                    <div className="w-1/3 hidden lg:block"></div>
 
-                    </div>
-                    <div className='w-1/2' >
-                        <h1 className='text-white text-[40px]  ' >Would you like a Cup of Delicious Coffee?</h1>
-                        <p className='text-white text-justify ' >It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
-                        <button className='bg-[#E3B577] px-3 py-2  shadow-2xl ' >Learn More</button>
+                    {/* Content Section */}
+                    <div className="lg:w-1/2 w-full p-6 my-16 text-center lg:text-left">
+                        <h1 className="text-white text-[20px] lg:text-[40px]">
+                            Would you like a Cup of Delicious Coffee?
+                        </h1>
+                        <p className="text-white text-justify text-[10px] lg:text-[17px]">
+                            It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your
+                            companion of every moment!!! Enjoy the beautiful moments and make them memorable.
+                        </p>
+                        <button className="bg-[#E3B577] text-[10px] lg:text-[16x] px-3 my-2 py-2 shadow-2xl rounded-md text-white font-semibold hover:bg-[#c68c5c] transition-all">
+                            Learn More
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Banner
+export default Banner;
--- src/pages/home-page/SubBanner.jsx
+++ src/pages/home-page/SubBanner.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
+import React from 'react';
 
 const SubBanner = () => {
     return (
-        <div className=' bg-[#ECEAE3] ' >
-            <div className='max-w-4xl mx-auto  py-8 ' >
-                <div className='flex justify-center items-center gap-16 ' >
-                    {/* item 1 */}
-                    <div className='' >
-                        <img src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948695/1_1_ebz5io.png" alt="" />
-                        <h1  className='text-[#331A15] text-[30px] '  >Awesome Aroma</h1>
-                        <p className=' text-justify  text-[#1B1A1A] ' >You will definitely be a fan of the design & aroma of your coffee</p>
-                    </div>
+        <div className="bg-[#ECEAE3]">
+            <div className="max-w-4xl mx-auto py-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 text-center">
+                    {/* Item 1 */}
                     <div>
-                        <img src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948694/3_1_g3v3ze.png" alt="" />
-                        <h1 className='text-[#331A15] text-[30px] ' >High Quality</h1>
-                        <p className=' text-justify  text-[#1B1A1A] ' >We served the coffee to you maintaining the best quality</p>
+                        <img
+                            src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948695/1_1_ebz5io.png"
+                            alt="Awesome Aroma"
+                            className="mx-auto"
+                        />
+                        <h1 className="text-[#331A15] text-[24px] md:text-[30px] mt-4">Awesome Aroma</h1>
+                        <p className="text-[#1B1A1A] text-sm md:text-base">
+                            You will definitely be a fan of the design & aroma of your coffee
+                        </p>
                     </div>
+
+                    {/* Item 2 */}
                     <div>
-                        <img src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948695/2_1_egpxgl.png" alt="" />
-                        <h1 className='text-[#331A15] text-[30px] ' >Pure Grades</h1>
-                        <p className=' text-justify text-[#1B1A1A]  ' >The coffee is made of the green coffee beans which you will love</p>
+                        <img
+                            src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948694/3_1_g3v3ze.png"
+                            alt="High Quality"
+                            className="mx-auto"
+                        />
+                        <h1 className="text-[#331A15] text-[24px] md:text-[30px] mt-4">High Quality</h1>
+                        <p className="text-[#1B1A1A] text-sm md:text-base">
+                            We served the coffee to you maintaining the best quality
+                        </p>
                     </div>
+
+                    {/* Item 3 */}
                     <div>
-                        <img src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948694/4_1_qpwzuq.png" alt="" />
-                        <h1 className='text-[#331A15] text-[30px] ' >Proper Roasting</h1>
-                        <p className=' text-justify  text-[#1B1A1A]  ' >Your coffee is brewed by first roasting the green coffee beans</p>
+                        <img
+                            src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948695/2_1_egpxgl.png"
+                            alt="Pure Grades"
+                            className="mx-auto"
+                        />
+                        <h1 className="text-[#331A15] text-[24px] md:text-[30px] mt-4">Pure Grades</h1>
+                        <p className="text-[#1B1A1A] text-sm md:text-base">
+                            The coffee is made of the green coffee beans which you will love
+                        </p>
+                    </div>
+
+                    {/* Item 4 */}
+                    <div>
+                        <img
+                            src="https://res.cloudinary.com/dj2edy2rg/image/upload/v1731948694/4_1_qpwzuq.png"
+                            alt="Proper Roasting"
+                            className="mx-auto"
+                        />
+                        <h1 className="text-[#331A15] text-[24px] md:text-[30px] mt-4">Proper Roasting</h1>
+                        <p className="text-[#1B1A1A] text-sm md:text-base">
+                            Your coffee is brewed by first roasting the green coffee beans
+                        </p>
                     </div>
                 </div>
             </div>
-        </div> 
-    )
-}
+        </div>
+    );
+};
 
-export default SubBanner
+export default SubBanner;
